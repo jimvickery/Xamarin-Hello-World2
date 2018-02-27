921495def6783a301a3f5f75b497e999e568aa1c
Jim Vickery <jimrv8@yahoo.com>
2018-02-27 03:08:46 -0600

    Label opacity is slider responsive now.

--- HelloWorld2/GreetPage.xaml.cs
+++ HelloWorld2/GreetPage.xaml.cs
@@ -7,17 +7,17 @@ namespace HelloWorld2
 {
     public partial class GreetPage : ContentPage
     {
-       
+
+
 
         public GreetPage()
         {
             InitializeComponent();
+            slider.Value = 0.5;
+
         }
 
-        void Handle_Clicked(object sender, System.EventArgs e)
-        {
-            DisplayAlert("Title", "Testing While Watching TV", "OK");
-        }
 
+       
     }
 }
