bcc42e4a120459df910a12d78dd7690d89a73a04
Jim Vickery <jimrv8@yahoo.com>
2018-03-01 00:58:47 -0600

    Added Row Definitions

--- HelloWorld2/StackPage.xaml.cs
+++ HelloWorld2/StackPage.xaml.cs
@@ -10,6 +10,15 @@ namespace HelloWorld2
         public StackPage()
         {
             InitializeComponent();
+
+            //var layout = new StackLayout
+            //{
+            //    Spacing = 40,
+            //    Padding = new Thickness(0, 20, 0, 0),
+            //    Orientation = StackOrientation.Horizontal
+            //};
+            //layout.Children.Add(new Label {Text = "Label 1"});
+            //Content = layout;
         }
     }
 }
