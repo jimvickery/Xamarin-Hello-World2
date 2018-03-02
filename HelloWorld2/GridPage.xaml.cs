971ec4dfef3f8a9e7118e9656b3fada0c5bc9b25
Jim Vickery <jimrv8@yahoo.com>
2018-03-02 00:45:17 -0600

    Completed Grid Exercise 1

--- HelloWorld2/GridPage.xaml.cs
+++ HelloWorld2/GridPage.xaml.cs
@@ -11,33 +11,33 @@ namespace HelloWorld2
         {
             InitializeComponent();
 
-            var grid = new Grid
-            {
-                RowSpacing = 20,
-                ColumnSpacing = 40
-            };
+            //var grid = new Grid
+            //{
+            //    RowSpacing = 20,
+            //    ColumnSpacing = 40
+            //};
 
-            var label = new Label { Text = "Label 1" };
-            grid.Children.Add(label, 0, 0 );
-            Grid.SetRowSpan(label, 2);
-            Grid.SetColumnSpan(label, 2);
-            Grid.SetRow(label, 0);
-            Grid.SetColumn(label, 0);
+            //var label = new Label { Text = "Label 1" };
+            //grid.Children.Add(label, 0, 0 );
+            //Grid.SetRowSpan(label, 2);
+            //Grid.SetColumnSpan(label, 2);
+            //Grid.SetRow(label, 0);
+            //Grid.SetColumn(label, 0);
 
-            grid.RowDefinitions.Add(new RowDefinition
-            {
-                Height = new  GridLength(100, GridUnitType.Absolute)
-            });
+            //grid.RowDefinitions.Add(new RowDefinition
+            //{
+            //    Height = new  GridLength(100, GridUnitType.Absolute)
+            //});
 
-            grid.RowDefinitions.Add(new RowDefinition
-            {
-                Height = new GridLength(2, GridUnitType.Star)
-            });
+            //grid.RowDefinitions.Add(new RowDefinition
+            //{
+            //    Height = new GridLength(2, GridUnitType.Star)
+            //});
 
-            grid.RowDefinitions.Add(new RowDefinition
-            {
-                Height = new GridLength(1, GridUnitType.Auto)
-            });
+            //grid.RowDefinitions.Add(new RowDefinition
+            //{
+            //    Height = new GridLength(1, GridUnitType.Auto)
+            //});
         }
     }
 }
